14449a500a736f2c3569a6f73bf28033c9e10a8a
George Goldberg <george@gberg.me>
2019-09-16 16:23:40 +0100

    Initial commit; naive implementation.

---
+++ index.js
@@ -0,0 +1,27 @@
+export default class Client {
+    constructor(server, key) {
+        this.server = server;
+        this.key = key;
+    };
+
+    track = (event, data) => {
+        let payload = {
+            source: this.key,
+            schema: event,
+            client_timestamp: (new Date()).getTime(),
+            data: data,
+        };
+        return fetch(this.server, {
+            method: 'POST',
+            mode: 'no-cors',
+            cache: 'no-cache',
+            credentials: 'omit',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            redirect: 'follow',
+            referrer: 'no-referrer',
+            body: JSON.stringify(payload),
+        });
+    };
+}
