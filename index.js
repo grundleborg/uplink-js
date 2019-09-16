4014b90e7686a3b074df2be641f9d39949cc9d4d
George Goldberg <george@gberg.me>
2019-09-16 16:51:58 +0100

    Add a warehouse variable.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 export default class Client {
-    constructor(server, key) {
+    constructor(server, warehouse, key) {
         this.server = server;
+        this.warehouse = warehouse;
         this.key = key;
     };
 
     track(event, data) {
         let payload = {
+            warehouse: this.warehouse,
             source: this.key,
             schema: event,
             client_timestamp: (new Date()).getTime(),
